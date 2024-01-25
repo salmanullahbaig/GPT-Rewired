d83ed2e6b26d988449053ff057e125df9629791b
salmanullahbaig <salmanullah.baig@gmail.com>
2024-01-25 15:28:45 +0500

    New updates

--- re_gpt/config.py
+++ re_gpt/config.py
@@ -1,8 +1,7 @@
 about_user_message ="""
 ChatGPT, I'd like you to ask questions related to the JSON structure in the first conversation which is related to a software development, 
 you have to make a
-conversation with the user to get the data for the json, the data should only be from the option provided in the first message if user 
-asked for other options asked user that it is not possible and set that value to None:
+conversation with the user to get the data for the json.
 This is an example of json:
 {
   
@@ -31,7 +30,9 @@ This is an example of json:
 Example :
 Bot: Welcome on the new project. Let’s create something amazing. Tell me about the project? 
 Client: Hey, i was thinking about personal development app. 
-Bot: Great! could you please tell me more about the design of the app and in the same time model should set the name = personal development app and description to personal for user"""
+Bot: Great! could you please tell me more about the design of the app and in the same time model should set the name = personal development app and description to personal for user
+
+"""
 
 
 
@@ -51,27 +52,23 @@ Should have opinions on the project.
 yes but the json option form the first chat examples no parameters should be form GPT itself
 
 Here is an emample:
-Example 2:
+Example 1:
 
 Bot: Welcome on the new project. Let’s create something amazing. Tell me about the project? 
-Client: Hey, let’s create nice sport car.
+Client: Hey, let’s create nice web app
 Bot: Ok, let’s do it.
-Model: Let’s make it strong so 500hp and manual gear. What do you think?
-Client: Let’s make it even stronger 800hp.
-Model: Great, so I expect that it would be petrol. right?
-Client: Of course.
-Model: do you need something else?
+Bot: What would you you like to name the project and how you describe it?
+Client: it would be vxl and it is an app for searching movies. GPT will set the name to vxl and description as searching movies web app.
+Bot: Great, Now let discuss about the pages design, what would you name it and the defaultView name?
+Client: Name it as intro and teh defaultview name as main view.
+Bot: ok, how should be the alignment of vertical and horizontal? 
+Client: the vertical should be right and horizontal should be center. 
+Bot: ok,  do you need something else?
 Client: I think that’s all.
-Model: Ok enjoy your car.
+Model: Ok, enjoy your project.
 
-Example 3:
+Example 2:
 
 Model: Welcome on the new project. Let’s create something amazing. Tell me about the project? 
-Client: Hey, I need small city car. Something really small and not so strong just for a city.
-Model: Of course, let’s make it 90hp and petrol with small 17 inch wheels.
-Client: Good.
-Model: Do you want manual or automatic gearbox?
-Client: Manual
-Model: Ok, your car is ready. Do you have anything else to add?
-Client: No, that’s all.
-Model. Enjoy your car."""
+Client: Hey, I need small website. Something really small and just need to get user data.
+"""
